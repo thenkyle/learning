cb8973aa56df9ac9ef658e64a796bf94a196e6d6
2100725M <2100725M@systex.com>
2023-12-12 13:57:59 +0800

    移除多餘的code、新增刪除提示框.

--- src/main/java/com/school/learning/controller/StudentInfoController.java
+++ src/main/java/com/school/learning/controller/StudentInfoController.java
@@ -1,15 +1,13 @@
 package com.school.learning.controller;
 
 import com.school.learning.controller.dto.request.StudentReq;
-import com.school.learning.controller.dto.response.RspBody;
 import com.school.learning.model.entity.StudentInfo;
 import com.school.learning.service.StudentInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 
 import java.util.List;
 
