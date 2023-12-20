89078c74c89ae35a2b7c1a263b109a61cd1e7d5e
2100725M <2100725M@systex.com>
2023-12-20 16:45:32 +0800

    feature: 1.properties add cross configuration. 2.Controller add CrossOrigin Annotation.

--- src/main/java/com/school/learning/controller/StudentInfoController.java
+++ src/main/java/com/school/learning/controller/StudentInfoController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/learning/student")
+@CrossOrigin
 public class StudentInfoController {
     @Autowired
     private StudentInfoService studentInfoService;
